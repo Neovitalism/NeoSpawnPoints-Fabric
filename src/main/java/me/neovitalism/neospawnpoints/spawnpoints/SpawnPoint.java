285b7466869d76ae84624b75c845d6efa893209b
Neovitalism <70066544+Neovitalism@users.noreply.github.com>
2025-03-14 20:32:51 -0500

    Fix rotation not working on firstspawn

--- src/main/java/me/neovitalism/neospawnpoints/spawnpoints/SpawnPoint.java
+++ src/main/java/me/neovitalism/neospawnpoints/spawnpoints/SpawnPoint.java
@@ -4,6 +4,7 @@ import me.neovitalism.neoapi.config.Configuration;
 import me.neovitalism.neoapi.objects.Location;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtDouble;
+import net.minecraft.nbt.NbtFloat;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.server.network.ServerPlayerEntity;
 
@@ -39,11 +40,17 @@ public class SpawnPoint extends Location {
     public void setNBTLocation(NbtCompound nbtCompound) {
         if (nbtCompound == null) nbtCompound = new NbtCompound();
         nbtCompound.putString("Dimension", this.getWorld().getRegistryKey().getValue().toString());
-        NbtList listTag = new NbtList();
-        listTag.addElement(0, NbtDouble.of(this.getX()));
-        listTag.addElement(1, NbtDouble.of(this.getY()));
-        listTag.addElement(2, NbtDouble.of(this.getZ()));
-        nbtCompound.put("Pos", listTag);
+        NbtList posTag = new NbtList();
+        posTag.addElement(0, NbtDouble.of(this.getX()));
+        posTag.addElement(1, NbtDouble.of(this.getY()));
+        posTag.addElement(2, NbtDouble.of(this.getZ()));
+        nbtCompound.put("Pos", posTag);
+        if (this.getPitch() != -1000 && this.getYaw() != -1000) {
+            NbtList rotationTag = new NbtList();
+            rotationTag.addElement(0, NbtFloat.of(this.getYaw()));
+            rotationTag.addElement(1, NbtFloat.of(this.getPitch()));
+            nbtCompound.put("Rotation", rotationTag);
+        }
     }
 
     @Override
